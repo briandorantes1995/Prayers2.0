12c13438895bfa5b93c0d486227ba02c087c5386
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-04-18 22:06:07 -0600

    Se agrego autenticacion con firebase y logeo

---
+++ prayers2.0/src/Components/Login.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, signInWithEmailAndPassword, signInWithGoogle } from '../firebaseConfig';
+import { useAuthState } from "react-firebase-hooks/auth";
+import './Registro.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate();
+    useEffect(() => {
+    }, [navigate,user, loading]);
+    return (
+        <div className="align">
+            <div className="grid align__item">
+                <div className="register">
+                    <svg className="site__logo" width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 9.47256V6.26286C4 5.92383 4.21369 5.62162 4.53332 5.50861L7.73332 4.3772C8.25389 4.19314 8.80587 4.5903 8.9971 5.10828C9.44791 6.32939 10.6223 7.20005 12 7.20005C13.3777 7.20005 14.5521 6.3294 15.0029 5.10828C15.1941 4.5903 15.7461 4.19314 16.2667 4.3772L19.4667 5.50861C19.7863 5.62163 20 5.92383 20 6.26286V9.47256C20 9.99301 19.5109 10.3749 19.006 10.2487L17.794 9.94569C17.2891 9.81946 16.8 10.2013 16.8 10.7218V19.2972C16.8 19.739 16.4418 20.0972 16 20.0972H8C7.55817 20.0972 7.2 19.739 7.2 19.2972V10.7218C7.2 10.2013 6.71089 9.81946 6.20597 9.94569L4.99403 10.2487C4.48911 10.3749 4 9.99302 4 9.47256Z" stroke="#4f7a28" stroke-width="0.8"></path> </g></svg>
+                    <h2>Iniciar Sesion</h2>
+                    <div className="form">
+                        <div className="form__field">
+                            <label>Usuario:</label>
+                            <input type="email" value={email} placeholder="info@mailaddress.com" onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div className="form__field">
+                            <label>Contraseña: </label>
+                            <input type="password" value={password} placeholder="••••••••••••" onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <button className="btn-primary" onClick={() => signInWithEmailAndPassword(email, password)} >Iniciar Sesion </button>
+                        <button className="btn-primary" onClick={signInWithGoogle}>Inicia Sesion con Google</button>
+                    </div>
+                        No tienes Cuenta? <Link to="/registro">Registrate</Link> ahora.
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default Login
--- prayers2.0/src/firebaseConfig.js
+++ prayers2.0/src/firebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {GoogleAuthProvider,getAuth,signInWithPopup,signInWithEmailAndPassword,createUserWithEmailAndPassword,sendPasswordResetEmail,signOut} from "firebase/auth";
+import { getFirestore,query, collection, getDocs, where, addDoc } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,85 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//All the auth done
+const auth = getAuth(app);
+//database for the project
+const db = getFirestore(app);
+
+
+//Google Auth
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+        if (docs.docs.length === 0) {
+            await addDoc(collection(db, "users"), {
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email,
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+
+//Email Auth
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+//Register User
+const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "users"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+//Reset Password
+const sendPasswordReset = async (email) => {
+    try {
+        await sendPasswordResetEmail(auth, email);
+        alert("Password reset link sent!");
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+//Log Out function
+const logout = () => {
+    signOut(auth);
+};
+
+export {
+    auth,
+    db,
+    signInWithGoogle,
+    logInWithEmailAndPassword,
+    registerWithEmailAndPassword,
+    sendPasswordReset,
+    logout,
+    signInWithEmailAndPassword,
+};
